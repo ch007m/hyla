98d9ce813e4d4bd2ce3b6dbc3fc119947cd34949
Charles Mouliard <ch007m@gmail.com>
2013-12-07 17:54:53 +0100

    Change version to 1.0

--- lib/hyla/project.rb
+++ lib/hyla/project.rb
@@ -1,5 +1,5 @@
 module Hyla
-    VERSION = '0.1.0'
+    VERSION = '1.0'
     DESCRIPTION = 'Asciidoctor Hyla - Command Line tool to create new project, watch modifications, generate content, publish or consult it live !'
     SUMMARY = 'Asciidoctor Hyla - builder/generator of HTML5, slideshow. Watch modifications, generate content, publish or consult it live !'
 end

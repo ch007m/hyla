b20716b652be82f0c258c8ef2059f805193f4c75
Charles Moulliard <ch007m@gmail.com>
2014-01-31 12:35:09 +0100

    Add comment and link to install LiveReload, fixed issue #28

--- lib/hyla/commands/watch.rb
+++ lib/hyla/commands/watch.rb
@@ -123,11 +123,11 @@ module Hyla
         dir_file = File.dirname(f)
         file_to_process = Pathname.new(f).basename
         @ext_name = File.extname(file_to_process)
-        Hyla.logger.info ">> Directory of the file to be processed : #{dir_file}"
+        Hyla.logger.info ">> Directory of the file(s) to be processed : #{dir_file}"
         Hyla.logger.info ">> File to be processed : #{file_to_process}"
         Hyla.logger.info ">> Extension of the file : #{@ext_name}"
 
-        if @ext_name != '.html'
+        if [".adoc",".ad",".asciidoc",".txt",".index"].include? @ext_name
           # Generate File name
           # Rename xxx.adoc, xxx.asciidoc, xxx.ad, xxx.index to xxx.html
           to_file = file_to_process.to_s.gsub(/.adoc|.ad|.asciidoc|.index/, '.html')

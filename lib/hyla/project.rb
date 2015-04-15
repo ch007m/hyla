237f9a0aec11ac0c41e934f9d5c5de7b08143f47
Charles Moulliard <ch007m@gmail.com>
2015-04-15 17:49:39 +0200

    Change version to pre.10

--- lib/hyla/project.rb
+++ lib/hyla/project.rb
@@ -1,5 +1,5 @@
 module Hyla
-    VERSION = '1.0.7.pre.9'
+    VERSION = '1.0.7.pre.10'
     DESCRIPTION = 'Asciidoctor Hyla - Command Line tool to create new project, watch modifications, generate content, publish or consult it live !'
     SUMMARY = 'Asciidoctor Hyla - builder/generator of HTML5, slideshow. Watch modifications, generate content, publish or consult it live !'
 end

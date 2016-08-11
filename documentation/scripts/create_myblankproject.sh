d216c5a0a38d264645ead305d06d1e9aae428666
Charles Moulliard <cmoulliard@apache.org>
2016-08-11 17:41:41 +0200

    Fix wrong command used

--- documentation/scripts/create_myblankproject.sh
+++ documentation/scripts/create_myblankproject.sh
@@ -3,10 +3,10 @@
 source remove_temp_directories.sh
 
 echo "Create a MyBlankProject"
-hyla new --blank -d ~/hyla/MyBlankProject
+hyla new -b ~/hyla/MyBlankProject
 
 echo "Create a MyBlankProject using --force option"
-hyla new -b -d ~/hyla/MyBlankProject --force
+hyla new -b ~/hyla/MyBlankProject --force
 
 echo "Project created"
 ls -la ~/hyla/MyBlankProject
